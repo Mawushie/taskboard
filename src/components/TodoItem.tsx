36b0089e3b420094ae784c5f8e464e3fcfba3757
Mawushie <afi.mawushie.honyah@gmail.com>
2024-09-11 22:20:47 +0000

    complete task and delete task functionalities done

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,23 +5,35 @@ import { IoCheckmark } from "react-icons/io5";
 
 interface Props {
   todo: Todo;
-  //   todos: Todo[];
-  //   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-export default function TodoItem({ todo }: Props) {
+export default function TodoItem({ todo, todos, setTodos }: Props) {
+  const handleCompleted = (id: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="flex justify-between items-center">
-      <div>{todo.todo}</div>
-      <div className="flex gap-1">
+      <div> {todo.isDone ? <s>{todo.todo}</s> : <span>{todo.todo}</span>}</div>
+      <div className="flex gap-3">
         {" "}
         <span>
           <CiEdit />
         </span>
-        <span>
+        <span onClick={() => handleDelete(todo.id)}>
           <MdDeleteOutline />
         </span>
-        <span>
+        <span onClick={() => handleCompleted(todo.id)}>
           <IoCheckmark />
         </span>
       </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,13 +4,13 @@ import TodoItem from "./TodoItem";
 //interface props
 interface Props {
   todos: Todo[];
-  //   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
-export default function TodoList({ todos }: Props) {
+export default function TodoList({ todos, setTodos }: Props) {
   return (
     <div className="flex flex-col  p-4">
       {todos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} />
+        <TodoItem todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );
